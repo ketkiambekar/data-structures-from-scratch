47fb5dd8b93b179590926214b9938cf35452105a
ketkiambekar <ambekar.ketki@gmail.com>
2021-04-09 20:26:25 -0400

    Changes

--- bubblesort.py
+++ bubblesort.py
@@ -5,8 +5,8 @@ def bubble_sort(arr):
     print(arr)
 
     #bubble sort loops
-    for i in range(l):
-        for j in range(l-1):
+    for i in range(l-1):
+        for j in range(l-i-1):
 
             #swap adjacent elements if they are unsorted
             if arr[j]>arr[j+1] :
@@ -18,5 +18,19 @@ def bubble_sort(arr):
 if __name__=="__main__":
     arr = [5,4,3,2,1]
     bubble_sort(arr)
+
+
+##############Output##############
+#   [5, 4, 3, 2, 1]
+#   [4, 5, 3, 2, 1]
+#   [4, 3, 5, 2, 1]
+#   [4, 3, 2, 5, 1]
+#   [4, 3, 2, 1, 5]
+#   [3, 4, 2, 1, 5]
+#   [3, 2, 4, 1, 5]
+#   [3, 2, 1, 4, 5]
+#   [2, 3, 1, 4, 5]
+#   [2, 1, 3, 4, 5]
+#   [1, 2, 3, 4, 5]
 
 
