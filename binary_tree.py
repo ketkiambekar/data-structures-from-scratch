496ce41807775bd48bc4b9c9c14caa984c70a51f
ketkiambekar <ambekar.ketki@gmail.com>
2021-05-26 14:13:32 -0400

    Added pre-order, post-order and in-order traversal

--- binary_tree.py
+++ binary_tree.py
@@ -15,12 +15,15 @@ class BTree:
         self.root = n
 
     def printTree(self):
+        '''
+        Prints the nodes of the tree in a pyramid format
+        '''
+        print("Printing Tree..." , end="\n")
         #Queue that contains all nodes
         q1 =[self.root, None]
         levels =0
 
-
-        #Populate the queue
+        #Populate the queue to be printed later.
         i=0
         while i<len(q1):
             current = q1[i]
@@ -49,13 +52,16 @@ class BTree:
                 print( n.val, end="  ")
             
         
-
-
-
-
-
-    
     def add(self, dataval):
+        '''
+        Inserts a node in a binary tree
+
+        Args:
+            val: Value of the Node
+            
+        Returns:
+            Nothing
+        '''
         pointer = self.root
         while True:
             if dataval >=pointer.val:
@@ -72,6 +78,67 @@ class BTree:
                     break
                 
         
+    def PostOrderTraversal(self, n=None):
+        '''
+        Prints nodes in the order: left child, right child, root.
+        Args:
+            n: Root node of the tree (optional)
+        '''
+        
+        #implement using recursion
+
+        if n==None:
+            n=self.root
+     
+        if n.left==None and n.right ==None:
+            print(n.val, end= " ")
+            return
+        else:
+            if n.left: self.PostOrderTraversal(n.left)
+            if n.right: self.PostOrderTraversal(n.right)
+            print (n.val, end=" ")
+            
+
+    
+    def PreOrderTraversal(self, n=None):
+        '''
+        Prints nodes in the order: current, left child, right child.
+        Args:
+            n: Root node of the tree (optional)
+        '''
+        
+        #implement using recursion
+        if n==None:
+            n=self.root
+     
+        if n.left==None and n.right ==None:
+            print(n.val, end= " ")
+            return
+        else:
+            print (n.val, end=" ")
+            if n.left: self.PreOrderTraversal(n.left)
+            if n.right: self.PreOrderTraversal(n.right)
+        
+    def InOrderTraversal(self, n=None):
+        '''
+        Prints nodes in the order: left child, current, right child.
+        Args:
+            n: Root node of the tree (optional)
+        '''
+        
+        #implement using recursion
+        if n==None:
+            n=self.root
+     
+        if n.left==None and n.right ==None:
+            print(n.val, end= " ")
+            return
+        else:
+            if n.left: self.InOrderTraversal(n.left)
+            print (n.val, end=" ")
+            if n.right: self.InOrderTraversal(n.right)
+                    
+        
 def main():
     mBT=BTree(5)
     mBT.add(3)
@@ -84,14 +151,36 @@ def main():
     mBT.add(7)
     mBT.printTree()
 
+    print("Pre Order Traversal...\n")
+    mBT.PreOrderTraversal()
+
+    print("\n\nPost Order Traversal...\n")
+    mBT.PostOrderTraversal()
+
+    print("\n\nIn Order Traversal...\n")
+    mBT.InOrderTraversal()
+
 if __name__=="__main__":
     main()
 
 
 #Output 
+
+# Printing Tree...
 #        5  
 #       3  6  
 #      2  4  8  
 #     1  7  
 #    0  
   
+# Pre Order Traversal...
+
+# 5 3 2 1 0 4 6 8 7 
+
+# Post Order Traversal...
+
+# 0 1 2 4 3 7 8 6 5 
+
+# In Order Traversal...
+
+# 0 1 2 3 4 5 6 7 8 
