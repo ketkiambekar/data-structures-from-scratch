b384556dca12b0e7d0bcf2fa6b19470d30423688
ketkiambekar <ambekar.ketki@gmail.com>
2021-09-25 16:57:45 -0400

    Adding Mergesort.py

---
+++ MergeSort.py
@@ -0,0 +1,55 @@
+def mergeSort(arr):
+    """
+    Function to perform mergeSort recursively.
+    """
+
+    if len(arr)<=1: #terminating condition
+        return arr
+    else:
+        mid = len(arr)//2
+        Left = mergeSort(arr[:mid])
+        Right = mergeSort(arr[mid:])
+
+        return merge(Left, Right)
+
+
+def merge(Left, Right):
+    """
+    Function to merge two arrays in non decreasing order
+    """
+    
+    mergedArray=[]
+    i=j=0
+
+    while i < len(Left) and j< len(Right):
+        if Left[i]<= Right[j]:
+            mergedArray.append(Left[i])
+            i+=1
+        else:
+            mergedArray.append(Right[j])
+            j+=1
+    
+    #Append the remaining Array
+    if j < len(Right):
+        mergedArray+=Right[j:]
+    
+    if i < len(Left):
+        mergedArray+=Left[i:]
+
+    return mergedArray
+
+
+def main():
+    arr=[2,77,3,25,12,44,87,32,11]
+
+    print(mergeSort(arr))
+
+    print(mergeSort([0]))
+    
+    print(mergeSort([]))
+    
+    print(mergeSort([2,1]))
+
+main()
+
+
