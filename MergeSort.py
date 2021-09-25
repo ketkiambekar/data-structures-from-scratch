030e254fe45ed523020c36c85fe177096ee03c67
ketkiambekar <ambekar.ketki@gmail.com>
2021-09-25 17:12:51 -0400

    Added detailed output

--- MergeSort.py
+++ MergeSort.py
@@ -2,6 +2,7 @@ def mergeSort(arr):
     """
     Function to perform mergeSort recursively.
     """
+    print("Recursive call:", arr)
 
     if len(arr)<=1: #terminating condition
         return arr
@@ -9,7 +10,7 @@ def mergeSort(arr):
         mid = len(arr)//2
         Left = mergeSort(arr[:mid])
         Right = mergeSort(arr[mid:])
-
+        
         return merge(Left, Right)
 
 
@@ -17,7 +18,7 @@ def merge(Left, Right):
     """
     Function to merge two arrays in non decreasing order
     """
-    
+    print("Merging:", Left, Right)
     mergedArray=[]
     i=j=0
 
@@ -54,5 +55,34 @@ main()
 
 
 ######### OUTPUT ########
+
+# Recursive call: [2, 77, 3, 25, 12, 44, 87, 32, 11]
+# Recursive call: [2, 77, 3, 25]
+# Recursive call: [2, 77]
+# Recursive call: [2]
+# Recursive call: [77]
+# Merging: [2] [77]
+# Recursive call: [3, 25]
+# Recursive call: [3]
+# Recursive call: [25]
+# Merging: [3] [25]
+# Merging: [2, 77] [3, 25]
+# Recursive call: [12, 44, 87, 32, 11]
+# Recursive call: [12, 44]
+# Recursive call: [12]
+# Recursive call: [44]
+# Merging: [12] [44]
+# Recursive call: [87, 32, 11]
+# Recursive call: [87]
+# Recursive call: [32, 11]
+# Recursive call: [32]
+# Recursive call: [11]
+# Merging: [32] [11]
+# Merging: [87] [11, 32]
+# Merging: [12, 44] [11, 32, 87]
+# Merging: [2, 3, 25, 77] [11, 12, 32, 44, 87]
+
+
+
 
 # [2, 3, 11, 12, 25, 32, 44, 77, 87]
